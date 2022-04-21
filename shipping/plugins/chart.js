60004c688c06c65444511ea5291e5901cb040b7c
62160326 <60688879+ZeroNont@users.noreply.github.com>
2022-04-21 17:16:01 +0700

    shipping

---
+++ shipping/plugins/chart.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import HighchartsVue from "highcharts-vue";
+
+Vue.use(HighchartsVue);
+
+// import Vue from 'vue'
+// import { Line } from 'vue-chartjs'
+
+// Vue.component('line-chart', {
+// 	extends: Line,
+// 	props: ['data', 'options'],
+// 	mounted () {
+// 		this.renderChart(this.data, this.options)
+// 	}
+// })
